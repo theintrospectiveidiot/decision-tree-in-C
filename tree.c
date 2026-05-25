608694411816ff9893f7482de8cff627909ab7f3
theintrospectiveidiot <pbhaji57@gmail.com>
2026-05-26 01:15:10 +0530

    add make_thing, push_things

--- tree.c
+++ tree.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
 
 typedef struct thing {
     char *name;
@@ -10,10 +14,10 @@ typedef struct feature {
     char *name;
     int x_pos;
     
-    thing *dis_thing;
+    thing **dis_thing;
     int dis_total;
 
-    thing *thing;
+    thing **thing;
     int total;
 } feature;
 
@@ -31,18 +35,40 @@ struct branch {
 };
 
 feature *make_feature(char *name, int x, int dis_total, int total) {
-    feature *some_ftr = malloc(sizeof(feature));
+    feature *some_ftr = (feature *)malloc(sizeof(feature));
     
     some_ftr->name = strdup(name);
     some_ftr->x_pos = x;
     some_ftr->dis_total = dis_total;
-    some_ftr->dis_thing = malloc(sizeof(thing)*dis_total);
+    some_ftr->dis_thing = malloc(sizeof(thing *)*dis_total);
     some_ftr->total = total;
-    some_ftr->thing = malloc(sizeof(thing)*total);
+    some_ftr->thing = malloc(sizeof(thing *)*total);
 
     return some_ftr;
 }
 
-int main() {
+thing *make_thing(char *name, int *position, int index) {
+    thing *something = malloc(sizeof(thing));
+
+    something->name = strdup(name);
+    something->position = position;
+    something->index = index;
+
+    return something;
+}
+
+void push_things(feature *feature, ...) {
+    va_list args;
+
+    va_start(args, feature);
     
+    for(int i=0;i<feature->total;i++) {
+        feature->thing[i] = va_arg(args, thing *);
+    }
+
+    va_end(args);
+}
+
+int main() {
+    feature *ftr = make_feature("stuff", 0, 3, 6);
 }
