38a772e740f9fe926435100a1a3d75346252633b
theintrospectiveidiot <pbhaji57@gmail.com>
2026-05-26 19:07:43 +0530

    add compute_gini_overall

--- tree.c
+++ tree.c
@@ -1,19 +1,55 @@
 #include "tree.h"
 
-thing **give_arr(node *node, branch *b) {
-    if(strcmp(b->thing->name, node->feature->dis_thing[b->thing->index]->name)) {
-        printf("\nthe branch (%s) doesnt belong to the node [%s]\n", b->thing->name, node->feature->name);
+thing **give_arr(feature *feature, thing *th) {
+    if(strcmp(th->name, feature->dis_thing[th->index]->name)) {
+        printf("\nthe branch (%s) doesnt belong to the node [%s]\n", th->name, feature->name);
         exit(1);
     }
     int n = 0;
-    thing **arr = malloc(sizeof(thing *)*(node->feature->how_many_each[b->thing->index]));
-    for(int i=0;i<node->feature->total;i++) {
-        if (b->thing->index == node->feature->thing[i]->index) {
-            arr[n] = node->feature->thing[i];
+    thing **arr = malloc(sizeof(thing *)*(feature->how_many_each[th->index]));
+    for(int i=0;i<feature->total;i++) {
+        if (th->index == feature->thing[i]->index) {
+            arr[n] = feature->thing[i];
             n += 1;
         }
     }
     return arr;
+}
+
+double compute_gini_thingwise(thing **data, int *label, int how_many) {
+    
+    double yes = 0, no = 0;
+    double gini;
+
+    for(int i=0;i<how_many;i++) {
+        if (label[data[i]->position[1]]) {
+            yes += 1;
+            //printf("pos:%d\tit was yes\n", data[i]->position[1]);
+        }
+        else {
+            no += 1;
+            //printf("pos:%d\tit was no\n", data[i]->position[1]);
+        }
+    }
+
+    gini = 1 - (yes/how_many)*(yes/how_many) - (no/how_many)*(no/how_many);
+
+    return gini;
+}
+
+double compute_gini_overall(feature *ftr, int *label) {
+    
+    double gini_weighted = 0;
+
+    for(int i=0;i<ftr->dis_total;i++) {
+        thing **arr = give_arr(ftr, ftr->dis_thing[i]);
+        double ind_gi = compute_gini_thingwise(arr, label, ftr->how_many_each[i]);
+        gini_weighted += (((double) ftr->how_many_each[i]) / ((double) ftr->total)) * ind_gi;
+        //printf("%s's ind gi is %lf\n", ftr->dis_thing[i]->name, ind_gi);
+        free(arr);
+    }
+
+    return gini_weighted;
 }
 
 int main() {
@@ -53,9 +89,12 @@ int main() {
 
    int label[5] = {1, 0, 1, 0, 1};
 
-   thing **things = give_arr(root, root->branch[0]);
-   for(int i=0;i<root->feature->how_many_each[0];i++) {
+   thing **things = give_arr(stuff, stuff->dis_thing[1]);
+   for(int i=0;i<root->feature->how_many_each[1];i++) {
        printf("\nname: %s\npos: %d\tindex: %d\n", things[i]->name, things[i]->position[1], things[i]->index);
    } 
-   
+    
+   //printf("%s's gini impurity indivisually is %lf\n", stuff->dis_thing[1]->name, compute_gini_thingwise(give_arr(stuff, stuff->dis_thing[1]), label,stuff->how_many_each[1]));
+    
+    printf("%s's weighted mean gini impurity is %lf\n", stuff->name, compute_gini_overall(stuff, label));
 }
